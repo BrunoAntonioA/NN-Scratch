c1c776fb38df289aa6d6c39dc317348db90b5761
brunoantonioa <bruno.aguilar.b@mail.pucv.cl>
2020-10-22 15:19:26 -0300

    2

--- nn-scratch.py
+++ nn-scratch.py
@@ -1,5 +1,19 @@
 import numpy as np
 
+#Neuron Class
+class Neuron:
+    def __init__(self, neurons, weights, value, activationFunction):
+        self.neurons = neurons
+        self.weights = weights
+        self.value = value
+        self.activationFunction = activationFunction
+
+    def calculateOutput(self):
+        sum = 0
+        for i in range(len(self.neurons)):
+            sum += self.neurons[i] * self.weights[i]
+        return self.activationFunction(sum)
+
 def sigmoid(x, derivada=False):
     if derivada:
         return sigmoid(x)*(1-sigmoid(x)) 
--- red.py
+++ red.py
@@ -1,54 +1,40 @@
 import math
+import numpy as np
 import random
 
 #Neuron Class
 class Neuron:
-    def __init__(self, neurons, weights, value, activationFunction):
-        self.neurons = neurons
-        self.weights = weights
+    def __init__(self, previousLayer, previousLayerWeights, value, activationFunction):
+        self.output = 0
         self.value = value
         self.activationFunction = activationFunction
+        self.previousLayer = previousLayer
+        self.previousLayerWeights = previousLayerWeights
+        self.nextLayer = []
     
-    def calculateOutput(self):
-        sum = 0
-        for i in range(len(self.neurons)):
-            sum += self.neurons[i] * self.weights[i]
-        return self.activationFunction(sum)
 
 #Neural Network Class
 class NeuralNetwork:
-    def __init__(self, input_shape):
-        self.input_shape = input_shape
-        self.layers = []
+    # Where n is the input_layer len and m is the output_layer len
+    def __init__(self, n, m):
+        self.input_layer = np.empty(n)
+        self.output_layer = np.empty(m)
+        self.hidden_layers = []
+                
+    def forward(self, x):
+        if len(self.hidden_layers) != 0:
+            self.input_layer = x
 
-    # Adding new layer
-    def addLayer(self, n, activationFunction):
-        layer = []
-        # Adding n neurons
-        for i in range(n):
-            neuron = Neuron([], [], 0, activationFunction)
-            layer.append(neuron)
-            # If there is more than an input layer
-            if len(self.layers) > 1:
-                # l -> length of the previous layer
-                l = len(self.layers)
-                # m -> amount of neurons of the previous layer
-                m = len(self.layers[ l-1 ])
-                # Referencing previous layer to the i neuron of the new layer
-                neuron.neurons = self.layers[ l-1 ]
-                # Initialize weights of i neuron of the new layer
-                for j in range(m):
-                    neuron.weights.append(random.randint(0, 20))
-        # Appending the new layer to the NN
-        self.layers.append(layer)
+        else:
+            print("Is not possible to use the neural network without hidden layers")    
 
 # Sigmoid function
-def sigmoidFunction(x):
-    return ( 1 / (1 + math.exp(-x) ) )
+def sigmoid(x, derivada=False):
+    if derivada:
+        return sigmoid(x)*(1-sigmoid(x)) 
+    return 1 / (1 + np.exp(-x))
 
-nn = NeuralNetwork(9)
-nn.addLayer(10, sigmoidFunction)
-print("nn: ", nn.layers)
+
 
 
 
